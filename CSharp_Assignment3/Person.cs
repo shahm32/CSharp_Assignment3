f91ba6c9024e70d62495cba1b89955e51c7a8d94
shahm32 <shahm32@SCE043>
2024-03-26 18:10:18 -0600

    Add project files.

---
+++ CSharp_Assignment3/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Assignment3
+{
+    public class Person : BaseClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Person(string firstName = "", string lastName = "")
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override bool IsValidated()
+        {
+            if (FirstName == null)
+                FirstName = "";
+            if (LastName == null)
+                LastName = "";
+            return FirstName.Length>0&&LastName.Length>0; 
+        }
+    }
+}
---
+++ CSharp_Assignment3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Assignment3
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Person bob=new Person();
+            Console.WriteLine(bob.IsValidated());
+
+            Person kim=new Person("Kim","Smith");
+            Console.WriteLine(kim.IsValidated());
+        }
+    }
+}
